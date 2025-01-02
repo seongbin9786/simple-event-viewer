c8f73c7f50403e10582ea4c7fa6bd756bd794982
Seongbin Kim <seongbin9786@gmail.com>
2025-01-02 18:34:12 +0900

    fix: project 선택 변경 시 페이지네이션 관련 버그 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { Project } from "@buf/alignai_frontend-challenge-datetz.bufbuild_es/event/v1/event_pb";
 import { EventService } from "@buf/alignai_frontend-challenge-datetz.connectrpc_es/event/v1/event_connect";
@@ -10,7 +10,6 @@ import { createConnectTransport } from "@connectrpc/connect-web";
 import { useFetch, useTokenPaginatedFetch } from "@/utils/hooks";
 
 import { EventTable, EventViewerLayout, ProjectSelect } from "./modules";
-import { PaginatedEventList } from "./modules/PaginatedEventList";
 
 const transport = createConnectTransport({
   baseUrl:
@@ -40,13 +39,18 @@ export default function EventViewerPage() {
 
   const [currentPage, setCurrentPage] = useState(1);
 
-  console.log("currentPage:", currentPage);
+  useEffect(() => {
+    setCurrentPage(1);
+    resetEventsFetch();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [selectedProject]);
 
   const {
     data: listEventsResponse,
     isLoading: isEventsLoading,
     isEnabled: isEventsEnabled,
     totalPages: totalEventPages,
+    reset: resetEventsFetch,
   } = useTokenPaginatedFetch({
     enabled: !!selectedProject,
     pageSize: EVENTS_PAGE_SIZE,
@@ -56,9 +60,6 @@ export default function EventViewerPage() {
         projectId: selectedProject?.id,
         pageToken: nextPageToken,
       });
-
-      console.log("from server:", response);
-      console.log("from server - next token:", response.nextPageToken);
 
       const { events, totalSize } = response;
       return {
@@ -70,46 +71,14 @@ export default function EventViewerPage() {
         pageToken: response.nextPageToken,
       };
     },
-    deps: [selectedProject],
+    deps: [],
   });
 
   if (!isProjectEnabled || isProjectLoading) {
     return <div>Loading projects...</div>;
   }
 
-  console.log("projects:", projects);
-
-  if (!selectedProject || !isEventsEnabled || isEventsLoading) {
-    return (
-      <EventViewerLayout
-        projectSelect={
-          <ProjectSelect
-            projectList={projects}
-            selectedProject={selectedProject}
-            onSelect={setSelectedProject}
-          />
-        }
-        dateRangePicker={"(dateRangePicker)"}
-        eventTable={"Please choose a project to see events."}
-      />
-    );
-  }
-
-  const paginatedEventList: PaginatedEventList = {
-    project: selectedProject,
-    events: listEventsResponse.events,
-    totalEvents: listEventsResponse.totalSize,
-    pageSize: EVENTS_PAGE_SIZE,
-    currentPage,
-    hasNextPage: currentPage < totalEventPages,
-    goToPrevPage: () => setCurrentPage(Math.max(currentPage - 1, 1)),
-    goToNextPage: () =>
-      setCurrentPage(Math.min(currentPage + 1, totalEventPages)),
-  };
-
-  console.log("paginatedEventList:", paginatedEventList);
-  console.log("totalEventPages:", totalEventPages);
-  console.log("hasNextPage:", currentPage === totalEventPages);
+  const isEventsLoaded = selectedProject && isEventsEnabled && !isEventsLoading;
 
   return (
     <EventViewerLayout
@@ -121,7 +90,26 @@ export default function EventViewerPage() {
         />
       }
       dateRangePicker={"(dateRangePicker)"}
-      eventTable={<EventTable eventList={paginatedEventList} />}
+      eventTable={
+        <EventTable
+          eventList={
+            isEventsLoaded
+              ? {
+                  project: selectedProject,
+                  events: listEventsResponse.events,
+                  totalEvents: listEventsResponse.totalSize,
+                  pageSize: EVENTS_PAGE_SIZE,
+                  currentPage,
+                  hasNextPage: currentPage < totalEventPages,
+                  goToPrevPage: () =>
+                    setCurrentPage(Math.max(currentPage - 1, 1)),
+                  goToNextPage: () =>
+                    setCurrentPage(Math.min(currentPage + 1, totalEventPages)),
+                }
+              : undefined
+          }
+        />
+      }
     />
   );
 }
--- src/utils/hooks/useTokenPaginatedFetch.ts
+++ src/utils/hooks/useTokenPaginatedFetch.ts
@@ -5,6 +5,7 @@ export interface PaginatedResult<ResultType> {
   isLoading: boolean;
   isEnabled: boolean;
   totalPages: number;
+  reset: () => void;
 }
 
 type PaginatedResultType<ResultType> = {
@@ -22,11 +23,6 @@ interface FetchProps<ResultType> {
   deps?: unknown[];
 }
 
-/*
-- currentPage가 올라가면 기존에 저장해둔 nextPageToken을 사용
-- currentPage가 내려가면 기존에 저장해둔 previousPageToken을 사용
-- 이렇게 되면, 탐색했던 페이지라는 개념이 필요함
-*/
 export const useTokenPaginatedFetch = <ResultType>({
   enabled = true,
   initialData = undefined,
@@ -35,24 +31,26 @@ export const useTokenPaginatedFetch = <ResultType>({
   fetchFn,
   deps = [],
 }: FetchProps<ResultType>): PaginatedResult<ResultType> => {
-  // currentPage에 대한 데이터만 보관.
   // TODO: page 별 데이터로 보관해 cache 기능 제공
   const [fetchResult, setFetchResult] = useState<ResultType | undefined>(
     initialData,
   );
   const [error, setError] = useState<unknown | undefined>(undefined);
   const [totalPages, setTotalPages] = useState(0);
-  const [visitedPage, setVisitedPage] = useState(1);
+  const [visitedPage, setVisitedPage] = useState(0);
   const [pageTokens, setPageTokens] = useState([""]);
 
-  console.log("[pag] pageTokens:", pageTokens);
-  console.log("[pag] visitedPage:", visitedPage);
+  const reset = () => {
+    setFetchResult(undefined);
+    setError(undefined);
+    setVisitedPage(0);
+    setTotalPages(0);
+    setPageTokens([""]);
+  };
 
   // pageSize 변경 시 초기화 필요
   useEffect(() => {
-    setError(undefined);
-    setVisitedPage(1);
-    setPageTokens([""]);
+    reset();
   }, [pageSize]);
 
   useEffect(() => {
@@ -93,6 +91,7 @@ export const useTokenPaginatedFetch = <ResultType>({
       isLoading: false,
       isEnabled: false,
       totalPages: 0,
+      reset,
     } as PaginatedResult<ResultType>;
   }
 
@@ -102,11 +101,13 @@ export const useTokenPaginatedFetch = <ResultType>({
         isLoading: true,
         isEnabled: true,
         totalPages,
+        reset,
       } as PaginatedResult<ResultType>)
     : ({
         data: fetchResult,
         isLoading: false,
         isEnabled: true,
         totalPages,
+        reset,
       } as PaginatedResult<ResultType>);
 };
