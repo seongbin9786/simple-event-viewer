95252e6761b47cb8c01c97afb7960af229800ade
Seongbin Kim <seongbin9786@gmail.com>
2025-01-02 13:21:46 +0900

    feat: project API 연동 코드 작성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,45 @@
 "use client";
 
+import { useState } from "react";
+
+import { EventService } from "@buf/alignai_frontend-challenge-datetz.connectrpc_es/event/v1/event_connect";
+import { createClient } from "@connectrpc/connect";
+import { createConnectTransport } from "@connectrpc/connect-web";
+
+import { useFetch } from "@/utils/hooks";
+
 import { EventTable, EventViewerLayout } from "./modules";
 
+const transport = createConnectTransport({
+  baseUrl:
+    "https://frontend-challenge-datetz-backend-725853975024.asia-northeast3.run.app/",
+});
+
+const client = createClient(EventService, transport);
+
 export default function EventViewerPage() {
+  const [selectedProjectId, setSelectedProjectId] = useState<
+    string | undefined
+  >(undefined);
+  const selectProjectById = (projectId: string) => {
+    console.log("selected projectId:", projectId);
+    setSelectedProjectId(projectId);
+  };
+
+  const { data: projects, isLoading: isProjectLoading } = useFetch({
+    fetchFn: async () => {
+      const result = await client.listProjects({});
+      return result.projects;
+    },
+    deps: [],
+  });
+
+  if (isProjectLoading) {
+    return <div>Loading...</div>;
+  }
+
+  console.log("projects:", projects);
+
   return (
     <EventViewerLayout
       projectSelect={"(Select)"}
